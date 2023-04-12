60a3f73a5e53bc7100f16a761fa325c803dcb3b7
ILM71 <93961632+ILM71@users.noreply.github.com>
2023-04-12 16:34:18 +0700

    Add files via upload

---
+++ gpt.py
@@ -0,0 +1,24 @@
+import openai
+
+openai.api_key = "api_chat_gpt_kalian"
+
+while True:
+    model_engine = "text-davinci-003"
+    
+    prompt = input('ketik pertanyaan anda : ')
+
+    if 'exit' in prompt or 'quit' in prompt:
+        break
+
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    response = completion.choices[0].text
+
+    print(response)
